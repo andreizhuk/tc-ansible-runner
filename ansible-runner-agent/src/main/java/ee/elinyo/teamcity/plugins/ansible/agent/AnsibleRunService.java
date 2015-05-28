59c69a804ead65eabfd0498210de0594e9c4514b
Andrei Zhuk <andrei.zhuk@easypark.net>
2015-05-29 00:35:31 +0300

    fix echo command

--- ansible-runner-agent/src/main/java/ee/elinyo/teamcity/plugins/ansible/agent/AnsibleRunService.java
+++ ansible-runner-agent/src/main/java/ee/elinyo/teamcity/plugins/ansible/agent/AnsibleRunService.java
@@ -3,11 +3,11 @@ package ee.elinyo.teamcity.plugins.ansible.agent;
 import java.util.ArrayList;
 import java.util.List;
 
-import ee.elinyo.teamcity.plugins.ansible.common.AnsibleRunnerConstants;
 import jetbrains.buildServer.RunBuildException;
 import jetbrains.buildServer.agent.runner.BuildServiceAdapter;
 import jetbrains.buildServer.agent.runner.ProgramCommandLine;
 import jetbrains.buildServer.agent.runner.SimpleProgramCommandLine;
+import ee.elinyo.teamcity.plugins.ansible.common.AnsibleRunnerConstants;
 
 public class AnsibleRunService extends BuildServiceAdapter {
 
@@ -15,12 +15,17 @@ public class AnsibleRunService extends BuildServiceAdapter {
     public ProgramCommandLine makeProgramCommandLine() throws RunBuildException {
         final String workingDir = getWorkingDirectory().getPath();
         
-        List<String> arguments = new ArrayList<String>();
-        arguments.add(getRunnerParameters().get(AnsibleRunnerConstants.PLAYBOOK_FILE_KEY));
-        arguments.add(getRunnerParameters().get(AnsibleRunnerConstants.COMMAND_TYPE_KEY));
-        arguments.add(getRunnerParameters().get(AnsibleRunnerConstants.SOURCE_CODE_KEY));
+        StringBuilder arg = new StringBuilder("");
+        arg.append(getRunnerParameters().get(AnsibleRunnerConstants.PLAYBOOK_FILE_KEY));
+        arg.append(getRunnerParameters().get(AnsibleRunnerConstants.COMMAND_TYPE_KEY));
+        arg.append(getRunnerParameters().get(AnsibleRunnerConstants.SOURCE_CODE_KEY));
         
-        return new SimpleProgramCommandLine(getEnvironmentVariables(), workingDir, "echo", arguments);
+        List<String> args = new ArrayList<String>();
+        args.add("/c");
+        args.add("echo");
+        args.add(arg.toString());
+        
+        return new SimpleProgramCommandLine(getEnvironmentVariables(), workingDir, "cmd", args);
     }
 
 }
