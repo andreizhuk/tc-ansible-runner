1c238d2a86b442db8622503a42a5efbaf11af9f1
Andrei Zhuk <andrei.zhuk@easypark.net>
2015-06-02 01:24:18 +0300

    recaps summary

--- ansible-runner-server/src/main/resources/buildServerResources/js/app.js
+++ ansible-runner-server/src/main/resources/buildServerResources/js/app.js
@@ -6,8 +6,43 @@ var AnsibleRunReport = (function() {
 				+ "/artifacts/content/.teamcity/ansible-run/ansible-run-report.json";
 		$j.getJSON(url, _generateReport);
 	}
+	
+	var _totalRecap = {
+		ok: 0,
+		failed: 0,
+		changed: 0,
+		unreachable: 0
+	};
+	
+	var _getTotalRecap = function(playbook) {
+		var playbookRecap = {
+			ok: 0,
+			failed: 0,
+			changed: 0,
+			unreachable: 0
+		}
+		$j.each(playbook.recaps, function(idx, recap) {
+			playbookRecap.ok += recap.statusSummary.ok;
+			_totalRecap.ok += recap.statusSummary.ok;
+			playbookRecap.failed += recap.statusSummary.failed;
+			_totalRecap.failed += recap.statusSummary.failed;
+			playbookRecap.changed += recap.statusSummary.changed;
+			_totalRecap.changed += recap.statusSummary.changed;
+			playbookRecap.unreachable += recap.statusSummary.unreachable;
+			_totalRecap.unreachable += recap.statusSummary.unreachable;
+		});
+		return playbookRecap;
+	}
+	
+	var _prepareModel = function(data) {
+		$j.each(data.playbooks, function(idx, playbook) {
+			playbook.totalRecap = _getTotalRecap(playbook);
+		});
+		data.totalRecap = _totalRecap;
+	}
 
 	var _generateReport = function(data) {
+		_prepareModel(data);
 		$j.get('/plugins/ansible-runner/html/report.mst', function(template) {
 			var rendered = Mustache.render(template, data);
 			$j('#ar_results_container').html(rendered);
